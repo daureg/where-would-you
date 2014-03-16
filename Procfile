9bf2eb284edf96f3c01da5852d2ff1e46296ed4f
Géraud Le Falher <daureg@gmail.com>
2014-03-16 13:34:42 +0200

    allow flask debug locally

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn hello:app
+web: ./run
--- hello.py
+++ hello.py
@@ -1,10 +1,15 @@
 #! /usr/bin/python2
 # vim: set fileencoding=utf-8
+import os
 from flask import Flask
 
 app = Flask(__name__)
+app.config['DEBUG'] = os.environ.get('DEBUG', False)
 
 
 @app.route('/')
 def hello():
     return 'Hello World!'
+
+if __name__ == '__main__':
+    app.run()
