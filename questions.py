3a2125a1e30d76bcef8d1891fd865c86f800e6b7
Géraud Le Falher <daureg@gmail.com>
2014-04-05 21:15:20 +0300

    be more selective for cultural venue

--- questions.py
+++ questions.py
@@ -21,7 +21,10 @@ QUESTIONS = OrderedDict([
     ('sport', Question(['Outdoors & Recreation'],
                        "as said, I already have the body of a Greek statue",
                        'run in the nature')),
-    ('culture', Question(['Arts & Entertainment'],
+    ('culture', Question(['Art Gallery', 'Comedy Club', 'Concert Hall',
+                          'Country Dance Club', 'Historic Site', 'Museum',
+                          'Movie Theater', 'Music Venue', 'Outdoor Sculpture',
+                          'Performing Arts Venue', 'Public Art', 'Street Art'],
                          "I simply watch TV",
                          'enjoy some cultural attractions')),
 ])
